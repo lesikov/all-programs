b16a9d9ec3b147a8c4b243bd399a5051ebda1a90
lesikov <lesikov.denis@mail.ru>
2013-07-05 19:14:11 +0400

    Иная строковая интерполяция. Строковая интерполяция без значений по ключу

--- tdd/deposit.py
+++ tdd/deposit.py
@@ -16,10 +16,10 @@ class DepositInformer:
     Для получения актуального курса валют информатор использует сервис
     https://openexchangerates.org, который предоставляет данные в формате JSON.
     """
-    _URL_OEC = 'http://openexchangerates.org/api/latest.json?app_id={app_id}'
+    _URL_OEC = 'http://openexchangerates.org/api/latest.json?app_id={}'
 
     def __init__(self, init_deposit=None, debug=False):
-        self.url = self._URL_OEC.format(app_id=OPENEXCHANGERATES_APP_ID)
+        self.url = self._URL_OEC.format(OPENEXCHANGERATES_APP_ID)
 
         if (init_deposit):
             self.deposit = init_deposit
