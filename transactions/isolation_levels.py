d407d1bb8379b540a7f90f86baee92af42d126e3
Lesikov Denis <lesikov.denis@mail.ru>
2013-07-06 21:04:09 +0400

    Добавлен вывод строки из базы до совершения изменений

--- transactions/isolation_levels.py
+++ transactions/isolation_levels.py
@@ -7,22 +7,38 @@ import time
 import sqlite3
 import threading
 
-
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s (%(threadName)-10s) %(message)s')
+
+
+#  Вывод строки до изменений
+with sqlite3.connect('bank.db') as conn:
+    c = conn.cursor()
+    c.execute('SELECT * FROM donation')
+    result = c.fetchone()
+    c.close()
+
+    print('BEFORE CHANGES:')
+    print(result, end='\n\n')
+
 
 def writer():
     logging.debug('connecting')
     with sqlite3.connect('bank.db') as conn:
         c = conn.cursor()
         logging.debug('connected')
+
+        # Изменения значения даты на текущую локальную
         c.execute('UPDATE donation SET donate_on=DATETIME("now", "localtime")')
         logging.debug('changes made')
+
         logging.debug('waiting to synchronize')
         ready.wait() # синхронизация
         logging.debug('PAUSING')
         time.sleep(1)
+
+        # Применение изменений в базу данных
         conn.commit()
         logging.debug('CHANGES COMMITTED')
     return
@@ -30,14 +46,18 @@ def writer():
 def reader():
     with sqlite3.connect('bank.db') as conn:
         c = conn.cursor()
+
         logging.debug('waiting to synchronize')
         ready.wait() # синхронизация
         logging.debug('wait over')
+
+        # Выборка строк в потоке (в это время в потоке Writer была начата транзакция и выполнено изменение)
         c.execute('SELECT * from donation')
         logging.debug('SELECT EXECUTED')
-        results = c.fetchall()
-        logging.debug(results)
-        logging.debug('results fetched')
+        result = c.fetchone()
+        # В потоке Writer транзакция на это время не применена
+        logging.debug('result fetched')
+        logging.debug(result)
     return
 
 
