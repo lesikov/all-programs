2d29323de3eb073cd2f1b3a4279549ba4af35c59
Lesikov Denis <lesikov.denis@mail.ru>
2013-07-06 12:16:59 +0400

    Обновлена схема: добавлен пол донора

--- transactions/bank_schema.sql
+++ transactions/bank_schema.sql
@@ -2,8 +2,9 @@
 
 -- Информация и сдаче крови в банк
 CREATE TABLE donation (
-    id         integer primary key autoincrement not null,
-    donate_on  date,
-    donor_name text,
-    blood_type text
+    id           integer primary key autoincrement not null,
+    donate_on    date,
+    donor_name   text,
+    donor_gender text,
+    blood_type   text
 );
